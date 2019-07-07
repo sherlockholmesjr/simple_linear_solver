a6c9f7bdfa327d2121c85646896d18dee85ee4db
sherlockholmesjr <muhammadadejr3@gmail.com>
2019-07-07 20:35:34 +0700

    commit

--- tutorial/magic_add.py
+++ tutorial/magic_add.py
@@ -5,7 +5,8 @@ class vector2d:
     def __add__(self, other):
         return vector2d(self.x + other.x, self.y + other.y)
 first = vector2d(1, 2)
+print(first.x, first.y)
 second = vector2d(3, 4)
+print(second.x, second.y)
 result = first + second
-print(result.x)
-print(result.y)
+print(result.x, result.y)
